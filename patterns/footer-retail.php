a78888eb7b16a5a343744da34d55e48528735a78
Chandan Kumar <93089403+chandan578@users.noreply.github.com>
2025-09-16 12:14:49 +0530

    add footer part

---
+++ patterns/footer-retail.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Title: Footer Retail
+ * Slug: Online-Grocery-Theme/footer-retail
+ * Categories: Footers
+ */
+?>
+<!-- wp:group {"style":{"color":{"background":"#212529"},"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}},"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"textColor":"base-2","layout":{"type":"constrained","contentSize":"100%"}} -->
+<div class="wp-block-group has-base-2-color has-text-color has-background has-link-color" style="background-color:#212529;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:columns {"verticalAlignment":null,"style":{"spacing":{"blockGap":{"top":"2rem","left":"2rem"},"padding":{"top":"0","bottom":"0"}}}} -->
+<div class="wp-block-columns" style="padding-top:0;padding-bottom:0"><!-- wp:column {"verticalAlignment":"top","width":"38%"} -->
+<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:38%"><!-- wp:site-logo {"width":200,"shouldSyncIcon":false,"align":"center","className":"is-style-rounded"} /-->
+
+<!-- wp:heading {"textAlign":"center","level":3,"style":{"color":{"text":"#198754"},"elements":{"link":{"color":{"text":"#198754"}}}}} -->
+<h3 class="wp-block-heading has-text-align-center has-text-color has-link-color" style="color:#198754"><strong>Online Grocery Theme</strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}},"layout":{"selfStretch":"fit","flexSize":null}},"textColor":"base-2"} -->
+<p class="has-text-align-center has-base-2-color has-text-color has-link-color">Your one-stop destination for fresh groceries, daily essentials, and household products delivered at your door. Your one-stop destination for fresh groceries, daily essentials, and household products delivered at your door.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"32%","style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30"}}}} -->
+<div class="wp-block-column" style="padding-right:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30);flex-basis:32%"><!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between","verticalAlignment":"top"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontSize":"1.5rem"},"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"textColor":"base-2"} -->
+<h3 class="wp-block-heading has-text-align-center has-base-2-color has-text-color has-link-color" style="font-size:1.5rem"><strong>Organic</strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:navigation {"ref":937,"overlayMenu":"never","style":{"typography":{"fontSize":"1rem"},"spacing":{"blockGap":"0.6rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<!-- wp:navigation-link {"label":"Home","url":"/shop"} /-->
+
+<!-- wp:navigation-link {"label":"About Us","url":"/about"} /-->
+
+<!-- wp:navigation-link {"label":"Contact Us","url":"/contact"} /-->
+
+<!-- wp:navigation-link {"label":"Blogs","url":"/blog"} /-->
+<!-- /wp:navigation --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":3,"style":{"typography":{"fontSize":"1.5rem"},"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"textColor":"base-2"} -->
+<h3 class="wp-block-heading has-text-align-center has-base-2-color has-text-color has-link-color" style="font-size:1.5rem"><strong>Quick Link</strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:navigation {"overlayMenu":"never","style":{"typography":{"fontSize":"1rem"},"spacing":{"blockGap":"0.6rem"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<!-- wp:navigation-link {"label":"Offer","url":"/shop"} /-->
+
+<!-- wp:navigation-link {"label":"Discount Coupons","url":"/shop"} /-->
+
+<!-- wp:navigation-link {"label":"Offline Stores","url":"/shop"} /-->
+
+<!-- wp:navigation-link {"label":"Latest Products","url":"/shop"} /-->
+
+<!-- wp:navigation-link {"label":"My Account","url":"/shop"} /-->
+<!-- /wp:navigation --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top","width":"30%"} -->
+<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:30%"><!-- wp:heading {"textAlign":"left","level":3,"style":{"typography":{"fontSize":"1.5rem"},"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"textColor":"base-2"} -->
+<h3 class="wp-block-heading has-text-align-left has-base-2-color has-text-color has-link-color" style="font-size:1.5rem"><strong>Contact US</strong></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"left","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base"} -->
+<p class="has-text-align-left has-base-color has-text-color has-link-color">📞 : +91 83609 94063</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"left","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base"} -->
+<p class="has-text-align-left has-base-color has-text-color has-link-color">📧 : info@kaddora.com</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"left","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base"} -->
+<p class="has-text-align-left has-base-color has-text-color has-link-color">📍 : 1st Floor B33 Noida Sector-63, 201301, Utter Pradesh</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><strong>KEEP IN TOUCH</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"customIconBackgroundColor":"#6bb252","iconBackgroundColorValue":"#3f8127ff"} -->
+<ul class="wp-block-social-links has-icon-background-color"><!-- wp:social-link {"url":"\u003c?php echo esc_url('https://www.instagram.com/kaddoratech/'); ?\u003e","service":"instagram","label":"Instagram"} /-->
+
+<!-- wp:social-link {"url":"\u003c?php echo esc_url('https://x.com/KaddoraTech'); ?\u003e","service":"x"} /-->
+
+<!-- wp:social-link {"url":"\u003c?php echo esc_url('https://kaddora.com/'); ?\u003e","service":"whatsapp"} /-->
+
+<!-- wp:social-link {"url":"\u003c?php echo esc_url('https://www.linkedin.com/company/kaddora-tech/posts/') ?\u003e","service":"linkedin"} /-->
+
+<!-- wp:social-link {"url":"\u003c?php echo esc_url('https://www.facebook.com/KaddoraTech'); ?\u003e","service":"facebook"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size"><strong>🪪 | FSSAI Certified</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size"><strong>🔏 | Secure Payment</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size"><strong>🌳| Eco-friendly Packaging</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:separator {"className":"is-style-wide","style":{"layout":{"selfStretch":"fit","flexSize":null}}} -->
+<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+<!-- /wp:separator --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|20","left":"var:preset|spacing|20"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignfull" style="padding-right:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size">©️ 2025 Kaddoar Tech. All rights reserved.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size">Made by ❤️ with Kaddora Tech</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">Designed with <a href="https://kaddora.com/" data-type="link" data-id="https://kaddora.com/" target="_blank" rel="noreferrer noopener">Kaddora Tech</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
