858b2b2fabfec5f0385579bfd93eee322f1dcf2d
Chandan Kumar <93089403+chandan578@users.noreply.github.com>
2025-09-16 18:49:05 +0530

    add banner page

---
+++ patterns/banner-retail-01.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Title: Banner Retail 01
+ * Slug: Online-Grocery-Theme/banner-retail-01
+ * Categories: front page
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10","left":"3rem","right":"3rem"}}},"backgroundColor":"base","layout":{"type":"default"}} -->
+<div class="wp-block-group has-base-background-color has-background" style="padding-top:var(--wp--preset--spacing--10);padding-right:3rem;padding-bottom:var(--wp--preset--spacing--10);padding-left:3rem"><!-- wp:columns {"verticalAlignment":null,"style":{"color":{"background":"#f1fdf6"},"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"},"blockGap":{"top":"var:preset|spacing|30","left":"var:preset|spacing|30"}}}} -->
+<div class="wp-block-columns has-background" style="background-color:#f1fdf6;padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"style":{"color":{"text":"#198754"},"elements":{"link":{"color":{"text":"#198754"}}}},"fontFamily":"system-sans-serif"} -->
+<h2 class="wp-block-heading has-text-color has-link-color has-system-sans-serif-font-family" style="color:#198754"><strong>Your Daily Essentials, Delivered</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Enjoy a curated selection of trusted brands and fresh products with easy online ordering and fast, reliable delivery that keeps your home stocked effortlessly.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"color":{"background":"#198754"},"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-background wp-element-button" style="background-color:#198754;padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10)"><strong>Shop Now</strong></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":960,"aspectRatio":"16/9","scale":"cover","sizeSlug":"large","linkDestination":"none","style":{"shadow":"var:preset|shadow|natural","border":{"radius":"25px"}}} -->
+<figure class="wp-block-image size-large has-custom-border"><img src="<?php echo esc_url(get_stylesheet_directory_uri() . "/assets/images/hero.jpeg");?>" alt="" class="wp-image-960" style="border-radius:25px;box-shadow:var(--wp--preset--shadow--natural);aspect-ratio:16/9;object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
