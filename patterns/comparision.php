354dbf7dbf5b93859adb975fa84abc1be5e77ba0
chandanKaddora <chandan@kaddora.com>
2025-09-22 15:34:38 +0530

    complete front page code

---
+++ patterns/comparision.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Title: Compareison & Our Ingredients
+ * Slug: Online-Grocery-Theme/comparision
+ * Categories: front page
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|30"}}},"layout":{"type":"constrained","contentSize":"100%"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--30)"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|20","right":"var:preset|spacing|20"},"margin":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}},"border":{"radius":"25px"},"color":{"background":"#def3e8"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group has-background" style="border-radius:25px;background-color:#def3e8;margin-top:var(--wp--preset--spacing--10);margin-bottom:var(--wp--preset--spacing--10);padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--20)"><!-- wp:heading {"textAlign":"center","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|10"}}}} -->
+<h2 class="wp-block-heading has-text-align-center" style="padding-bottom:var(--wp--preset--spacing--10)"><strong>Compare Our Packs</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:columns {"verticalAlignment":null,"style":{"spacing":{"padding":{"right":"var:preset|spacing|10","left":"var:preset|spacing|10"}}}} -->
+<div class="wp-block-columns" style="padding-right:var(--wp--preset--spacing--10);padding-left:var(--wp--preset--spacing--10)"><!-- wp:column {"width":"50%"} -->
+<div class="wp-block-column" style="flex-basis:50%"><!-- wp:image {"id":291,"sizeSlug":"full","linkDestination":"none","style":{"layout":{"columnSpan":1,"rowSpan":1},"border":{"radius":"25px"},"shadow":"var:preset|shadow|natural"}} -->
+<figure class="wp-block-image size-full has-custom-border"><img src="<?php echo esc_url(get_stylesheet_directory_uri() . "/assets/images/comparision.jpeg");?>" alt="" class="wp-image-291" style="border-radius:25px;box-shadow:var(--wp--preset--shadow--natural)"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"50%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%"><!-- wp:table {"className":"comparison-table is-style-stripes","style":{"color":{"background":"#cff8de"},"layout":{"columnSpan":1,"rowSpan":1}}} -->
+<figure class="wp-block-table comparison-table is-style-stripes"><table class="has-background has-fixed-layout" style="background-color:#cff8de"><thead><tr><th class="has-text-align-center" data-align="center">Pack Size</th><th class="has-text-align-center" data-align="center">Price</th><th class="has-text-align-center" data-align="center">Best For</th></tr></thead><tbody><tr><td class="has-text-align-center" data-align="center">250 g</td><td class="has-text-align-center" data-align="center">$5.99</td><td class="has-text-align-center" data-align="center">First-time try</td></tr><tr><td class="has-text-align-center" data-align="center">500 g</td><td class="has-text-align-center" data-align="center">$9.99</td><td class="has-text-align-center" data-align="center">Daily use</td></tr><tr><td class="has-text-align-center" data-align="center">1 Kg</td><td class="has-text-align-center" data-align="center">$18.99</td><td class="has-text-align-center" data-align="center">Family pack</td></tr><tr><td class="has-text-align-center" data-align="center">2 Kg</td><td class="has-text-align-center" data-align="center">$34.99</td><td class="has-text-align-center" data-align="center">Big families</td></tr><tr><td class="has-text-align-center" data-align="center">5 Kg</td><td class="has-text-align-center" data-align="center">$ 79.99</td><td class="has-text-align-center" data-align="center">Bulk buyers / parties</td></tr><tr><td class="has-text-align-center" data-align="center">10 Kg</td><td class="has-text-align-center" data-align="center">$149.99</td><td class="has-text-align-center" data-align="center">Restaurants &amp; catering</td></tr></tbody></table></figure>
+<!-- /wp:table --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"10px","bottom":"40px"}}},"layout":{"type":"constrained","contentSize":"1200px"}} -->
+<div class="wp-block-group" style="padding-top:10px;padding-bottom:40px"><!-- wp:heading {"textAlign":"center","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|10"}}},"fontFamily":"system-serif"} -->
+<h2 class="wp-block-heading has-text-align-center has-system-serif-font-family" style="padding-bottom:var(--wp--preset--spacing--10)"><strong>Where Our Ingredients Come From</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"layout":{"type":"grid","minimumColumnWidth":"20rem"}} -->
+<div class="wp-block-group"><!-- wp:cover {"url":"<?php echo esc_url(get_stylesheet_directory_uri() . "/assets/images/gradient1.png");?>","id":217,"dimRatio":50,"customOverlayColor":"#8a6f61","isUserOverlayColor":false,"sizeSlug":"full","layout":{"type":"constrained","contentSize":"100%"}} -->
+<div class="wp-block-cover"><img class="wp-block-cover__image-background wp-image-217 size-full" alt="" src="<?php echo esc_url(get_stylesheet_directory_uri() . "/assets/images/gradient1.png");?>" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim" style="background-color:#8a6f61"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size"><strong>🌾 Fresh from local farms</strong></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:cover {"url":"<?php echo esc_url(get_stylesheet_directory_uri() . "/assets/images/gradient2.png");?>","id":218,"dimRatio":50,"customOverlayColor":"#86804f","isUserOverlayColor":false,"sizeSlug":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover"><img class="wp-block-cover__image-background wp-image-218 size-full" alt="" src="<?php echo esc_url(get_stylesheet_directory_uri() . "/assets/images/gradient2.png");?>" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim" style="background-color:#86804f"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size"><strong>🥑 Cold-pressed natural oils</strong></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:cover {"url":"<?php echo esc_url(get_stylesheet_directory_uri() . "/assets/images/gradient3.png");?>","id":219,"dimRatio":50,"customOverlayColor":"#838c36","isUserOverlayColor":false,"isDark":false,"sizeSlug":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-light"><img class="wp-block-cover__image-background wp-image-219 size-full" alt="" src="<?php echo esc_url(get_stylesheet_directory_uri() . "/assets/images/gradient3.png");?>" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim" style="background-color:#838c36"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"large"} -->
+<p class="has-text-align-center has-base-color has-text-color has-link-color has-large-font-size"><strong>🌱 Handpicked organic herbs</strong></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
